402e55fb48df8b0b2d99b57ed254d057bb6b66a1
galkin_da2 <galkin_da2@nlmk.com>
2023-04-10 10:33:27 +0300

    Упрощение 2

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import {RouterProvider} from "react-router-dom";
+import reportWebVitals from './tools/reportWebVitals';
+import {createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
 import {QueryClient, QueryClientProvider,} from '@tanstack/react-query'
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
 import {LocalizationProvider} from '@mui/x-date-pickers';
-import {router} from "./App";
-import theme from "./theme";
+import theme from "./tools/theme";
 import CssBaseline from '@mui/material/CssBaseline';
 import {ThemeProvider} from '@mui/material/styles';
 import {GoogleOAuthProvider} from '@react-oauth/google';
 import 'dayjs/locale/ru';
+import {Route} from "react-router";
+import {AuthLayout} from "./layouts/AuthLayout";
+import Login from "./pages/Login";
+import Callback from "./pages/Callback";
+import {ProtectedLayout} from "./layouts/ProtectedLayout";
+import {MainLayout} from "./layouts/MainLayout";
+import Meets from "./pages/Meets";
+import Projects from "./pages/Projects";
+import Tasks from "./pages/Tasks";
+import Uniques from "./pages/Uniques";
+import ProjectDialog from "./pages/Project";
+import PlaceDialog from "./pages/Place";
 
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
+);
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/callback" element={<Callback />} />
+            <Route element={<ProtectedLayout />}>
+                <Route element={<MainLayout />}>
+                    <Route index element={<Meets />}/>
+                    <Route path="/projects" element={<Projects />}/>
+                    <Route path="/tasks" element={<Tasks />}/>
+                    <Route path="/uniques" element={<Uniques />}/>
+                </Route>
+                <Route path="/project/:id" element={<ProjectDialog />}/>
+                <Route path="/place/:id" element={<PlaceDialog />}/>
+            </Route>
+        </Route>
+    )
 );
 
 root.render(
