9b1d665232a6970e1906f27226d246444caeca2d
galkin_da2 <galkin_da2@nlmk.com>
2023-07-19 12:51:21 +0300

    Зачистка 2

--- app/src/router.tsx
+++ app/src/router.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import {createBrowserRouter, createRoutesFromElements} from "react-router-dom";
 import {Route} from "react-router";
-import AuthLayout from "./layouts/AuthLayout";
 import MeetsView from "./view/MeetsView";
 import MeetView from "./view/MeetView";
+import {useOutlet} from "react-router-dom";
+import {AuthProvider} from "./tools/auth";
+
+export default function AuthLayout(): JSX.Element {
+    const outlet = useOutlet();
+    return (
+        <AuthProvider>{outlet as JSX.Element}</AuthProvider>
+    );
+};
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
