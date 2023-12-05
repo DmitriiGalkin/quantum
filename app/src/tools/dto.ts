65b47aca66f676ac5a0cd6bce894a83f80752c4d
galkin_da2 <galkin_da2@nlmk.com>
2023-12-05 12:00:57 +0300

    Переработал БД

--- app/src/tools/dto.ts
+++ app/src/tools/dto.ts
@@ -1,56 +1,27 @@
-export interface Project {
-    id: number
-    title: string
-    description: string
-    image?: string
-    timing?: Timing[]
-    editable?: boolean // Право на редактирование/удаление
-    userId?: number // Организатор
-    latitude?: string
-    longitude?: string
-    ageFrom?: number // Минимальный возраст
-    ageTo?: number // Максимальный возраст
-    user?: User // Организатор проекта
-    place?: Place // Место проведения
+/**
+ * Родитель
+ */
+export interface Passport {
+    id: number // Идентификатор
+    title: string // Имя родителя
+    email: string | null // Почта
+    accessToken: string | null // Токен
 }
 
-export interface Meet {
-    id: number
-    // title: string
-    datetime: string
-    // description?: string
-    // image?: string
-    latitude: string
-    longitude: string
-    editable?: boolean // Право на редактирование/удаление
-    price?: number // Стоимость
-    // ageFrom?: number // Минимальный возраст
-    // ageTo?: number // Максимальный возраст
-    userMeet?: UserMeet // Участие пользователя
-    userMeets?: UserMeet[] // Все участники встречи
-    user?: User // Организатор встречи
-    place?: Place
-    project?: Project
-}
-
-export interface UserMeet extends User {
-    userId: number // Идентификатор участника
-    meetId: number // Идентификатор встречи
-    created?: string // Время вступления во встречу
-    started?: string // Время начала участия во встрече
-    stopped?: string // Время завершения участия во встрече
-    paided?: string // Время оплаты участия во встрече
-}
-
+/**
+ * Ребенок
+ */
 export interface User {
-    id: number
-    title: string
-    image: string
-    points: number
-    email?: string
-    password?: string
+    id: number // Идентификатор
+    passportId: number // Идентификатор паспорта родителя
+    title: string // Имя ребенка
+    age: number // Возраст ребенка
+    image: string | null // Фотография ребенка
 }
 
+/**
+ * Место проведения встречи
+ */
 export interface Place {
     id: number
     title: string
@@ -59,8 +30,71 @@ export interface Place {
     longitude: string
 }
 
-export interface Timing {
-    id: number
-    dayOfWeek: number
-    time: string
+/**
+ * Проект
+ */
+export interface Project {
+    id: number // Идентификатор
+    title: string // Название
+    description: string | null // Описание
+    image: string | null // Картинка проекта
+    userId: number // Инициатор
+    latitude: string // Широта, где потенциально проходит проект
+    longitude: string // Долгота, где потенциально проходит проект
+    ageFrom: number | null // Минимальный возраст
+    ageTo: number | null // Максимальный возраст
+
+    /**
+     * Дополнительные поля
+     */
+    // timing?: Timing[]
+    editable?: boolean // Право на редактирование/удаление
+    user?: User // Организатор проекта
+    place?: Place // Место проведения
 }
+
+/**
+ * Встреча
+ */
+export interface Meet {
+    id: number // Идентификатор
+    userId: number // Создатель
+    projectId: number // По какому проекту
+    datetime: string // Время проведения
+    latitude: string // Широта, где проходит встреча
+    longitude: string // Долгота, где проходит встреча
+    price: number | null // Стоимость
+
+    /**
+     * Дополнительные поля
+     */
+    // userMeet?: UserMeet // Участие пользователя
+    visits?: Visit[] // Все участники встречи
+    user?: User // Организатор встречи
+    place?: Place
+    project?: Project
+    editable?: boolean // Право на редактирование/удаление
+}
+
+/**
+ * Посещение
+ */
+export interface Visit {
+    userId: number // Идентификатор участника
+    meetId: number // Идентификатор встречи
+    created?: string // Время вступления во встречу
+    started?: string // Время начала участия во встрече
+    stopped?: string // Время завершения участия во встрече
+    paided?: string // Время оплаты участия во встрече
+
+    /**
+     * Дополнительные поля
+     */
+    user: User
+}
+
+// export interface Timing {
+//     id: number
+//     dayOfWeek: number
+//     time: string
+// }
